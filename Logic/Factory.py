07f6853d2d98f0e7e368bfaf0419a45b7f7c1b3f
Chase Pranga <chase.pranga@tuta.io>
2022-05-28 20:29:56 -0500

    Feature: implement Factory to produce workers

---
+++ Logic/Factory.py
@@ -0,0 +1,22 @@
+from Worker import Worker
+from Workers.User.GetTopSongs import GetTopSongs
+from Workers.Track.GetTopTags import GetTopTags
+
+
+class Factory:
+    def __init__(self, api_key):
+        self._api_key = api_key
+
+    def make_worker(self, type):
+        if type == 'track.gettoptags':
+            return GetTopTags(self._api_key)
+        elif type == 'user.gettoptracks':
+            return GetTopSongs(self._api_key)
+
+
+if __name__ == '__main__':
+    api_key = input('API Key: ')
+    username = input('Username: ')
+    factory = Factory(api_key)
+    worker = factory.make_worker('user.gettoptracks')
+    print(worker.make_call(username))
